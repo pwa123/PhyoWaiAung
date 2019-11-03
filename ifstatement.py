b8fe6a608dd8764cf055bb58234f2008d6a78b75
Phyo Wai Aung <phyowaiaung77aung@mail.com>
2019-11-03 17:46:57 +0630

    ifstatement

---
+++ ifstatement.py
@@ -0,0 +1,65 @@
+# x = int(input("Please enter an integer:"))
+
+# if x < 0:
+# 	x = 0
+# 	print('Negative changed to zero')
+# elif x == 0:
+# 	print('Zero')
+# elif x == 1:
+# 	print('Single')
+# else:
+# 	print('More')
+# # int - integer (Number)
+# str - strng 
+
+
+
+# x = int(input("Please enter an integer:"))
+
+# if x < 5:
+# 	x = 0
+# 	print('x is smaller than 5')
+# elif x == 7:
+# 	print('x is equal to 7')
+# elif x > 5 & x < 10:
+# 	print('x is between 5 & 10')
+# elif x > 10:
+# 	print('x is bigger than 10')
+# else:
+# 	print('x is undefined')
+
+
+# x = int(input("Please enter an integer:"))
+# if x < 19:
+# 	x = 1
+# 	print('Go to school')
+# elif x > 18 & x <= 50:
+# 	print('Go to Beach')
+# elif x < 50 & x <= 75:
+# 	print('Go to abbress')
+# elif x == 76:
+# 	print(''"I told that I don't like it"'')
+# else:
+# 	print('person is undefined')
+
+
+# words = ['cat', 'window', 'defenestrate']
+# for w in words:
+# 	print(w, len(w))
+
+
+
+# fruits = ['apple', 'banana', 'cucumber', 'pineapple', 'orange']
+
+# for i in range(5):
+# 	print(i)
+
+# a = ['Mary', '']
+
+
+
+b = ['father', 'mother', 'dauther', 'son', 'grandmother', 'grandmother']
+for c in range(len(b)):
+	print(c, c(i))
+
+
